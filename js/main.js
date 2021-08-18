fb0108cc36e3ea0243bbc1b2ef71f206eec33828
João Vitor Diniz <jdiniz@bwg.com.br>
2021-08-18 17:59:11 -0300

    Adicionando validação do IMC

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 var titulo = document.querySelector(".titulo")
 titulo.textContent = "App Nutricionista"
-console.log(titulo)
 
+var paciente = document.querySelector("#primeiro")
+var tdPeso = paciente.querySelector(".info-peso")
+var tdAltura = paciente.querySelector(".info-altura")
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    pesoEhValido= false
+    tdImc.textContent ="Peso Inválido"
+}
+if(altura <= 0 || altura >= 3){
+    alturaEhValida= false
+    tdImc.textContent= "Altura inválida"
+}
+
+if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
 

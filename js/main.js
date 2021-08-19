1562212d118a37131682810cbc38b2c4d4745b42
João Vitor Diniz <jdiniz@bwg.com.br>
2021-08-19 15:26:42 -0300

    Altração do css para registros inválidos

--- js/main.js
+++ js/main.js
@@ -1,28 +1,38 @@
 var titulo = document.querySelector(".titulo")
 titulo.textContent = "App Nutricionista"
 
-var paciente = document.querySelector("#primeiro")
-var tdPeso = paciente.querySelector(".info-peso")
-var tdAltura = paciente.querySelector(".info-altura")
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdImc = paciente.querySelector(".info-imc");
+for(var i = 0; i < pacientes.length ; i++){
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var tdAltura = paciente.querySelector(".info-altura");
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(peso <= 0 || peso >= 1000){
-    pesoEhValido= false
-    tdImc.textContent ="Peso Inválido"
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido");
+        tdImc.textContent = "Peso inválido!";
+        pesoEhValido = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(altura <= 0 || altura >= 3.0){
+        console.log("Altura inválida");
+        tdImc.textContent = "Altura inválida!";
+        alturaEhValida = false;
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if(pesoEhValido && alturaEhValida){
+        var imc = peso / ( altura * altura);    
+        tdImc.textContent = imc.toFixed(2);
+        
+    }    
 }
-if(altura <= 0 || altura >= 3){
-    alturaEhValida= false
-    tdImc.textContent= "Altura inválida"
-}
-
-if(alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc;
-}
-
